edd0ecba4eb7b3ea06d7125f1b7f3b568968ab52
AyushParmar9106 <143683235+AyushParmar9106@users.noreply.github.com>
2024-06-20 09:54:24 +0530

    Add files via upload

---
+++ Lab 3/Replace.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Replace {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of array: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            System.out.println("enter first number to swap: ");
+            int a=sc.nextInt();
+            System.out.println("enter second number to swap: ");
+            int b=sc.nextInt();
+        }
+        int temp=0;
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+}
