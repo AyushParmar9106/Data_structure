d47bfb3e1fde25fbff909df683e527a51a7f1cbf
AyushParmar9106 <143683235+AyushParmar9106@users.noreply.github.com>
2024-09-12 15:39:59 +1100

    Add files via upload

--- Lab 22/A_99.java
+++ Lab 22/A_99.java
@@ -38,7 +38,7 @@
             for(int i=0;i<n;i++)
             {
             
-                System.out.println(a[i]);
+                System.out.println("a[" +i+"]"+a[i]+" ");
             
             }
         }
